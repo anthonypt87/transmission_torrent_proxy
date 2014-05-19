a42a21c1f0dc289efe2aa25f53aeccc01544681e
anthony <anthonypt87@gmail.com>
2014-05-19 02:46:01 -0400

    messy but functional code

--- tests.py
+++ tests.py
@@ -1,20 +1,22 @@
+import mock
 import os
-import transmissionrpc
 import unittest
 
 import config
 import transmission_torrent_proxy
 
 
-class CanGetServerInformationIntegrationTest(unittest.TestCase):
+class TorrentProxyIntegrationTest(unittest.TestCase):
 
     def setUp(self):
         self.client = transmission_torrent_proxy.get_transmission_client()
         self.test_torrent_file = 'magnet:?xt=urn:btih:0e7439694e6fda56cd9c429fbff14feeab79f2b2&dn=The+Wolf+of+Wall+Street+-+Jordan+Belfort.epub&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337'
         self._wipe_test_torrent()
+        self._wipe_status_file()
 
     def tearDown(self):
         self._wipe_test_torrent()
+        self._wipe_status_file()
 
     def _wipe_test_torrent(self):
         torrents = self.client.get_torrents()
@@ -25,13 +27,18 @@ class CanGetServerInformationIntegrationTest(unittest.TestCase):
             ]:
                 self.client.remove_torrent([torrent.id], delete_data=True)
 
+    def _wipe_status_file(self):
+        if os.path.exists(config.status_file):
+            os.unlink(config.status_file)
+
     def test_adds_jobs_and_writes_file(self):
         ttp = transmission_torrent_proxy.TransmissionTorrentProxy(self.client)
         ttp.add_torrent(self.test_torrent_file)
+        self._assert_status_file_has_things()
 
+    def _assert_status_file_has_things(self):
         with open(config.status_file, 'r') as status_file:
             results = status_file.read()
-
         self.assertNotEqual(results, '')
 
     def test_doesnt_download_if_file_not_done(self):
@@ -42,9 +49,40 @@ class CanGetServerInformationIntegrationTest(unittest.TestCase):
             os.listdir(config.output_directory),
             []
         )
-        with open(config.status_file, 'r') as status_file:
-            results = status_file.read()
-        self.assertNotEqual(results, '')
+        self._assert_status_file_has_things()
+
+
+class TorrentProxyUnitTest(unittest.TestCase):
+
+    def test_downloads_file_if_done(self):
+        torrent_client = mock.Mock()
+        os_client = mock.Mock()
+        tp = transmission_torrent_proxy.TransmissionTorrentProxy(
+            torrent_client,
+            os_client
+        )
+
+        os_client.get_data_from_status_file.return_value = {
+            '1': transmission_torrent_proxy.Status.DOWNLOADING_TORRENT
+        }
+
+        torrent = mock.Mock(
+            id='1',
+            downloadDir='downloadDir',
+            status='seeding'
+        )
+        torrent.get_files.return_value = {0: {'name': 'name'}}
+        torrent_client.get_torrent.return_value = torrent
+
+        tp.download_results_if_any_done()
+
+        os_client.download.assert_called_once_with(
+            'downloadDir/name'
+        )
+        os_client.merge_status.assert_called_once_with(
+            {'1': transmission_torrent_proxy.Status.COMPLETE}
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
--- transmission_torrent_proxy.py
+++ transmission_torrent_proxy.py
@@ -1,7 +1,14 @@
+import subprocess
 import os
 import json
 import config
 import transmissionrpc
+
+
+class Status(object):
+    DOWNLOADING_TORRENT = 'downloading_torrent'
+    DOWNLOADING = 'downloading'
+    COMPLETE = 'download_complete'
 
 
 def get_transmission_client():
@@ -15,36 +22,74 @@ def get_transmission_client():
 
 class TransmissionTorrentProxy(object):
 
-    def __init__(self, client):
+    def __init__(
+        self,
+        client,
+        os_client=None
+    ):
         self._client = client
+        self._os_client = os_client or OSClient()
 
     def add_torrent(self, torrent_url):
         torrent = self._client.add_torrent(torrent_url)
-
-        with open(config.status_file, 'w') as status_file:
-            status_file.write(
-                json.dumps(
-                    {
-                        'id': torrent.id,
-                        'filename': torrent.name
-                    }
-                )
-            )
+        self._os_client.merge_status({
+            torrent.id: Status.DOWNLOADING_TORRENT
+        })
 
     def download_results_if_any_done(self):
-        self._create_download_directory_if_not_exists()
-        status = self._get_data_from_status_file()
+        self._os_client.create_download_directory_if_not_exists()
+        status = self._os_client.get_data_from_status_file()
         if not status:
             return
+        for torrent_id, torrent_status in status.iteritems():
+            if torrent_status == Status.DOWNLOADING_TORRENT:
+                torrent = self._client.get_torrent(torrent_id)
+                if torrent.status == 'seeding':
+                    for torrent_file in torrent.get_files().values():
+                        remote_filename = os.path.join(
+                            torrent.downloadDir,
+                            torrent_file['name']
+                        )
+                        self._os_client.download(remote_filename)
+                self._os_client.merge_status({torrent.id: Status.COMPLETE})
 
-    def _create_download_directory_if_not_exists(self):
-        if not os.path.exists(config.output_directory):
-            os.mkdir(config.output_directory)
 
-    def _get_data_from_status_file(self):
+class OSClient(object):
+
+    def get_data_from_status_file(self):
         with open(config.status_file, 'r') as status_file:
             raw_data = status_file.read()
             if not raw_data:
                 return
             else:
                 return json.loads(raw_data)
+
+    def create_download_directory_if_not_exists(self):
+        if not os.path.exists(config.output_directory):
+            os.mkdir(config.output_directory)
+
+    def merge_status(self, data):
+        with open(config.status_file, 'w+') as status_file:
+            raw_data = status_file.read()
+            if raw_data:
+                data_to_write = json.loads(raw_data)
+            else:
+                data_to_write = {}
+
+            data_to_write.update(data)
+
+            status_file.write(json.dumps(data))
+
+    def download(self, path):
+        args = [
+            'rsync',
+            'avz',
+            '%s@%s:%s' % (
+                config.username,
+                config.hostname,
+                path
+            ),
+            config.output_directory
+        ]
+
+        subprocess.call(args)
